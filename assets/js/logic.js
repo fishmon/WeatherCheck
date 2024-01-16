75089015576f66fac9c297135a8d15c780778212
Fishmon <serbanescuclaudiu93@gmail.com>
2024-01-16 16:23:44 +0000

    started working on js, dom elements done

---
+++ assets/js/logic.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // DOM elements
+    const enterCityInput = document.getElementById('enter-city');
+    const searchButton = document.getElementById('search-button');
+    const clearHistoryButton = document.getElementById('clear-history');
+    const historyForm = document.getElementById('history');
+    const todayWeatherCard = document.getElementById('today-weather');
+    const fiveDayForecastContainer = document.getElementById('five-day-forecast');
+  
